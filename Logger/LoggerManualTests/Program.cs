9cb81ed30cd9f436abfcffa98092f0a9e740238a
Michal Lánský <Lansky.michal3@gmail.com>
2020-05-08 14:04:34 +0200

    Added manual testsing project for ConsoleLogger.

---
+++ Logger/LoggerManualTests/Program.cs
@@ -0,0 +1,25 @@
+﻿using Logger.Loggers;
+using System;
+
+namespace LoggerManualTests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new ConsoleLogger();
+
+            logger.LogError("Error logged.");
+            Console.WriteLine("Empty");
+
+            logger.LogInfo("Info logged.");
+            Console.WriteLine("Empty");
+
+            logger.LogWarning("Warning logged.");
+            Console.WriteLine("Empty");
+
+            logger.LogFatal("FatalProblem.");
+            Console.WriteLine("Empty");
+        }
+    }
+}
