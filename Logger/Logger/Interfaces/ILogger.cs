1b86473df9dbbbe0d2dabd5437af11adf98b43d8
Michal Lánský <Lansky.michal3@gmail.com>
2020-05-08 13:17:34 +0200

    Created ILogger interface with shared logging functions.

---
+++ Logger/Logger/Interfaces/ILogger.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Logger.Interfaces
+{
+    /// <summary>
+    /// Interface for shared functions of logger.
+    /// </summary>
+    public interface ILogger
+    {
+        /// <summary>
+        /// Logs informative message.
+        /// </summary>
+        /// <param name="message">Text as string variable</param>
+        public void LogInfo(string message);
+
+        /// <summary>
+        /// Logs informative message from exception.
+        /// </summary>
+        /// <param name="exception">Exception to be log.</param>
+        public void LogInfo(Exception exception);
+
+        /// <summary>
+        /// Logs error message.
+        /// </summary>
+        /// <param name="message">Text as string variable</param>
+        public void LogError(string message);
+
+        /// <summary>
+        /// Logs error message from exception.
+        /// </summary>
+        /// <param name="exception">Exception to be log.</param>
+        public void LogError(Exception exception);
+
+        /// <summary>
+        /// Logs warning message.
+        /// </summary>
+        /// <param name="message">Text as string variable</param>
+        public void LogWarning(string message);
+
+        /// <summary>
+        /// Logs warning message from exception.
+        /// </summary>
+        /// <param name="exception">Exception to be log.</param>
+        public void LogWarning(Exception exception);
+
+        /// <summary>
+        /// Logs fatal message.
+        /// </summary>
+        /// <param name="message">Text as string variable</param>
+        public void LogFatal(string message);
+
+        /// <summary>
+        /// Logs fatal message from exception.
+        /// </summary>
+        /// <param name="exception">Exception to be log.</param>
+        public void LogFatal(Exception exception);
+    }
+}
